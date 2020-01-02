07ad167df4378ad620624e4c1a0ca7048737feeb
shengxiaoyi1993 <shengxiaoyi2014@icloud.com>
2020-01-02 21:02:24 +0800

    验证项目可用性

--- learn/exampleform/src/main.cpp
+++ learn/exampleform/src/main.cpp
@@ -1,69 +1,21 @@
-#include "opencv2/imgproc.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/core.hpp"
-#include"../../basic/src/basic.h"
-#include<iostream>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
 #include<string>
+#include<vector>
+#include <opencv2/features2d/features2d.hpp>
+// #include <opencv2/nonfree/nonfree.hpp>
 
 using namespace std;
 using namespace cv;
 
-int g_switch_value = 0;
-void switch_callback(int position);
 
-void  switch_off_function(){
-  std::cout<<"stitch off"<<std::endl;
-}
+int main(int argc, const char * argv[]) {
+  string path_image2="/home/sxy/Work/svn/bs2_repo/runway_scan/runway_scan_sw/splitline_detect/build/splitimage/blockcut_0_2_29.jpg";
+Mat mat2=imread(path_image2,IMREAD_GRAYSCALE);
+std::cout << "/* message */" << mat2.size()<<'\n';
 
-void switch_on_function(){
-  std::cout<<"stitch on"<<std::endl;
-}
 
-// void callback_add(int state, void* userData){
-//   cout<<__func__<<"state:"<<state<<endl;
-//
-// }
-// void callback_sub(int state, void* userData){
-//   cout<<__func__<<"state:"<<state<<endl;
-//
-// }
 
-int main(int argc,char** argv)
-{
-    cvNamedWindow("Example3_7",CV_WINDOW_NORMAL);
-    cvCreateTrackbar("bar","Example3_7",&g_switch_value,360,switch_callback);
-    // createButton("Add", callback_add, NULL, QT_PUSH_BUTTON, 0);
-    // createButton("Sub", callback_sub, NULL, QT_PUSH_BUTTON, 0);
-    cvWaitKey(0);
     return 0;
-}
-
-
-void switch_callback(int position)
-{
-  std::cout<<"position: "<<position<<std::endl;
-  cout<<g_switch_value<<endl;
-
-  string path_testimage="/home/sxy/Github/opencv_cpp/resource/2_3_609.jpg";
-  Mat mat_test=imread(path_testimage,IMREAD_GRAYSCALE);
-  resize(mat_test,mat_test,Size2i(1024,512));
-  cout<<mat_test.cols<<endl;
-  cout<<mat_test.rows<<endl;
-  int value_threshold=static_cast<int>(1.0*256*position/100);
-  cout<<"value_threshold:"<<value_threshold<<endl;
-  // threshold(mat_test,mat_test,value_threshold,255,CV_THRESH_BINARY_INV);
-  Mat mat_dst=basic::rotateAndCutImage(mat_test,position);
-
-  imshow("Example3_7",mat_dst);
-
-
-     if(position == 0)
-     {
-          switch_off_function();
-     }
-     else
-     {
-          switch_on_function();
-     }
 }
