1601fb7c08660bd52b2585b59403446a5b0e981d
qc_da4e05189d814e28850771a1831dbc61 <shengxiaoyi2014@icloud.com>
2022-11-29 21:01:52 +0800

    add CardRecognize/

--- personal/CardRecognize/demos/call_lib/src/main.cpp
+++ personal/CardRecognize/demos/call_lib/src/main.cpp
@@ -1,1 +1,14 @@
 ﻿#include<CardRecognize/CardRecognize.h>
+
+
+int main(int argc, char *argv[])
+{
+  std::string path_image="";
+  /// 对图片做模板计算
+  std::string path_template="";
+  cv::Mat mat_template=cv::imread(path_template);
+  cv::Mat mat_mask;
+
+
+  return 0;
+}
